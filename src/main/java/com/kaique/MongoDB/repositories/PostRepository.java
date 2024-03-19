6949cb2a717430b926b6f4e90f683a5a8b106c82
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-19 20:32:01 -0300

    GET Post full search

--- src/main/java/com/kaique/MongoDB/repositories/PostRepository.java
+++ src/main/java/com/kaique/MongoDB/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.kaique.MongoDB.repositories;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,4 +13,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ { moment: {$gte: ?1} }, { moment: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
 }
